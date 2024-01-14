54f9ed8e6d84909b70be5a92d133345a1ba91ca1
Nicola Ortolani <ortonico820@gmail.com>
2024-01-14 13:50:25 +0100

    Progetto presto finito

--- main.js
+++ main.js
@@ -92,3 +92,26 @@ annunci.forEach((annuncio, i)=>{
         ultimiAnnunciWrapper.appendChild(colonna)
     }
 })
+
+
+// SWIPER 
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    naivgation:{
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+  });
